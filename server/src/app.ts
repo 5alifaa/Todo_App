f7ac0bd7d8049c06b677c2d5decfcf13843ad8e6
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-06 22:06:59 +0200

    add morgan middleware for logging and update API route handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import router from './routes/router';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api', router);
+app.use('/api', router);
 
 export default app;
