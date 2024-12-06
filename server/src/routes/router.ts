8396c1adcf3931d3e0838144393b22f95e4b8f5c
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-06 21:59:31 +0200

    add authentication routes to main router

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import authRouter from './authRouter';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('router.ts');
 });
+
+router.use('/auth', authRouter);
 
 export default router;
