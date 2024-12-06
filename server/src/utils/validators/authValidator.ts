fd430d4f1b29a4831234b791ce4e1dc038c34efb
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-06 23:21:51 +0200

    refactor registerValidator to use body() for improved validation and error messages

--- server/src/utils/validators/authValidator.ts
+++ server/src/utils/validators/authValidator.ts
@@ -1,19 +1,30 @@
-import { check } from 'express-validator';
+import { body } from 'express-validator';
 import { PrismaClient } from '@prisma/client';
-import validatorMiddleware from '../../middlewares/validatorMiddleware';
-import { RequestHandler } from 'express';
+import { validationMiddleware } from '../../middlewares/validatorMiddleware';
 
 const prisma = new PrismaClient();
 
-export const registerValidator: RequestHandler[] = [
-  check('name', 'Name is required').notEmpty().isLength({ min: 5, max: 50 }),
-  check(
-    'password',
-    'Please enter a password with 6 or more characters',
-  ).isLength({ min: 6, max: 20 }),
-  check('email', 'Please include a valid email')
+export const registerValidator = [
+  body('name')
+    .trim()
     .notEmpty()
+    .withMessage('Name is required')
+    .isLength({ min: 3 })
+    .withMessage('Name must be at least 3 characters long'),
+
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 6, max: 20 })
+    .withMessage('Password must be between 6 to 20 characters long'),
+
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
     .isEmail()
+    .withMessage('Invalid email format')
     .custom((email) =>
       prisma.user.findFirst({ where: { email } }).then((user) => {
         if (user) {
@@ -22,5 +33,5 @@ export const registerValidator: RequestHandler[] = [
       }),
     ),
 
-  validatorMiddleware,
+  validationMiddleware,
 ];
