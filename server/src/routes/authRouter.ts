21551c74c9b3e81362c851712bdc83f9cb6805c7
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-07 11:35:40 +0200

    add login route with validation to authRouter

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { register } from '../services/authService';
-import { registerValidator } from '../utils/validators/authValidator';
+import { login, register } from '../services/authService';
+import {
+  loginValidator,
+  registerValidator,
+} from '../utils/validators/authValidator';
 
-const router = express.Router();  // this router will be mounted at /api/auth
+const router = express.Router(); // this router will be mounted at /api/auth
 
-router.post('/register',registerValidator, register);
+router.post('/register', registerValidator, register);
+router.post('/login', loginValidator, login);
 
 export default router;
