d19d612bc0d3074980ab0c3d071065cab1a2f37a
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-07 13:59:31 +0200

    add setUserId middleware to attach user ID to request body and update task service methods for user-specific task management

--- server/src/services/taskService.ts
+++ server/src/services/taskService.ts
@@ -1,29 +1,63 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { CustomRequest } from '../utils/CustomTypes';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const setUserId = (
+  req: CustomRequest,
+  _res: Response,
+  next: NextFunction,
+) => {
+  req.body.userId = req.user?.id;
+  next();
+};
 
 // @desc    Get all tasks
 // @route   GET /api/tasks
-// @access  Public
+// @access  Private
 export const getTasks = async (req: Request, res: Response) => {
-  res.send("Get All Tasks");
-} 
+  res.send('Get All Tasks');
+};
 
 // @desc    Create a task
 // @route   POST /api/tasks
 // @access  Private
 export const createTask = async (req: Request, res: Response) => {
-  res.send("Create a Task");
-}
+  // 1. Get user input
+  const { title, userId } = req.body;
+  // 2. Validate user input (validation layer)
+  // 3. Save task to database
+  const user = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      tasks: {
+        create: {
+          title,
+        },
+      },
+    },
+    include: {
+      tasks: true,
+    },
+  });
+  const data = { tasks: user.tasks.reverse() };
+  // 4. Send response
+  res.status(201).json({ data });
+};
 
 // @desc    Update a task
 // @route   PUT /api/tasks/:id
 // @access  Private
 export const updateTask = async (req: Request, res: Response) => {
-  res.send("Update a Task");
-}
+  res.send('Update a Task');
+};
 
 // @desc    Delete a task
 // @route   DELETE /api/tasks/:id
 // @access  Private
 export const deleteTask = async (req: Request, res: Response) => {
-  res.send("Delete a Task");
-}
+  res.send('Delete a Task');
+};
