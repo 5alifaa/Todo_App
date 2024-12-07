cedf3bb2e081d6f711e7e51d82a79e3177cd5731
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-07 11:30:55 +0200

    refactor createToken utility to use ApiError for missing JWT_SECRET

--- server/src/utils/createToken.ts
+++ server/src/utils/createToken.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
+import ApiError from './ApiError';
 
 const createToken = (payload:object) => {
   if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET is not defined');
+    throw new ApiError(500, 'JWT_SECRET is missing');
   }
   return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
