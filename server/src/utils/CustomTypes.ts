7769d2acef9ed86382688ff330793ddcf1803b51
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-07 14:30:41 +0200

    refactor CustomTypes to enhance user and task interfaces for better type safety

--- server/src/utils/CustomTypes.ts
+++ server/src/utils/CustomTypes.ts
@@ -1,18 +1,22 @@
 import { Request } from 'express';
 
-export interface CustomRequest extends Request {
-  user?: {
-    id: string;
-    name: string;
-    email: string;
-    createdAt: Date;
-    // optional tasks property
-    tasks?: {
-      id: string;
-      title: string;
-      completed: boolean;
-      createdAt: Date;
-    }[];
-  };
+export interface task {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+  userId: string;
 }
 
+export interface user {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+  tasksIds?: string[];
+  tasks?: task[];
+}
+
+export interface CustomRequest extends Request {
+  user?: user;
+}
