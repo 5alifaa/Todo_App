8529ffb881c2df5c06c04cac3bbbf53b2afa2267
Mohamed Khalifa <7mdiano@gmail.com>
2024-12-07 13:09:02 +0200

    add task validators for retrieving and deleting tasks with user ID checks

--- server/src/utils/validators/taskValidator.ts
+++ server/src/utils/validators/taskValidator.ts
@@ -1,8 +1,14 @@
-import { body } from 'express-validator';
+import { body, check } from 'express-validator';
 import validationMiddleware from '../../middlewares/validatorMiddleware';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+export const getTasksValidator = [
+  check('userId').isMongoId().withMessage('User ID is invalid'),
+
+  validationMiddleware,
+];
 
 export const createTaskValidator = [
   body('title')
@@ -49,6 +55,37 @@ export const updateTaskValidator = [
     .isBoolean()
     .withMessage('Completed should be a boolean'),
   body('userId').notEmpty().withMessage('User ID is required'),
+
+  validationMiddleware,
+];
+
+export const deleteTaskValidator = [
+  check('id')
+    .isMongoId()
+    .withMessage('Task ID is invalid')
+    .custom((value, { req }) => {
+      prisma.task
+        .findUnique({
+          where: {
+            id: value,
+          },
+        })
+        .then((task) => {
+          // check if the task exists
+          if (!task) {
+            return Promise.reject(new Error('Task not found'));
+          }
+
+          // check if the task belongs to the user
+          if (task?.userId !== req.body.userId) {
+            return Promise.reject(
+              new Error('Task does not belong to the user'),
+            );
+          }
+        });
+    }),
+
+  check('userId').isMongoId().withMessage('User ID is invalid'),
 
   validationMiddleware,
 ];
